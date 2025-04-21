d33bd22416ac6e9cefb39a5e5d55d683ea3d71fc
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-04-21 19:22:48 -0400

    project renamed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngularEmployeeManagementSolution' title`, () => {
+  it(`should have the 'Angular-EmployeeManagementSolution' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularEmployeeManagementSolution');
+    expect(app.title).toEqual('Angular-EmployeeManagementSolution');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularEmployeeManagementSolution');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-EmployeeManagementSolution');
   });
 });
