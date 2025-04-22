2f2d27151cfe8ff07e24d5bfa9d3182bed73d623
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-04-21 22:58:51 -0400

    Code Reorg

--- src/app/pipe/curreny-formatter.pipe.ts
+++ src/app/pipe/curreny-formatter.pipe.ts
@@ -10,12 +10,12 @@ export class CurrenyFormatterPipe implements PipeTransform {
     console.log(old_value);   // INDIA3,461.46
     console.log(new_symbol);  // Rs.
 
-    if(old_value.substr(0, 3) === 'CANADA')
+    if(old_value.substr(0, 3) === 'CAN')
       return old_value.replace('CANADA', new_symbol);  //Rs. 3,461.46
     else if(old_value.substr(0, 3) === 'USA')
-      return old_value.replace('USA', new_symbol);  //Rs. 3,461.46
+      return old_value.replace('USA', new_symbol);     //Rs. 3,461.46
     else
-      return old_value.replace('INDIA', new_symbol);  //Rs. 3,461.46
+      return old_value.replace('INDIA', new_symbol);   //Rs. 3,461.46
   }
 
 }
